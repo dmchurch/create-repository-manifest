eb544c198d6dc0c706bc550f2a8e83e8c7055e0c
Danielle Church <dani.church@gmail.com>
2024-03-01 16:57:00 -0500

    Pulling in glob, adjusting tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,20 +7,19 @@
  */
 
 import * as core from '@actions/core'
+import * as glob from '@actions/glob'
 import * as main from '../src/main'
 
 // Mock the action's main function
 const runMock = jest.spyOn(main, 'run')
-
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
 
 // Mock the GitHub Actions core library
 let debugMock: jest.SpiedFunction<typeof core.debug>
 let errorMock: jest.SpiedFunction<typeof core.error>
 let getInputMock: jest.SpiedFunction<typeof core.getInput>
 let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
-let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
+// let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
+let globCreateMock: jest.SpiedFunction<typeof glob.create>
 
 describe('action', () => {
   beforeEach(() => {
@@ -30,60 +29,44 @@ describe('action', () => {
     errorMock = jest.spyOn(core, 'error').mockImplementation()
     getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
     setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
-    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+    // setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+    globCreateMock = jest.spyOn(glob, 'create').mockImplementation()
   })
 
-  it('sets the time output', async () => {
+  it('runs without failure', async () => {
     // Set the action's inputs as return values from core.getInput()
     getInputMock.mockImplementation(name => {
       switch (name) {
-        case 'milliseconds':
-          return '500'
+        case 'follow-symbolic-links':
+          return 'TRUE'
+        case 'file-patterns':
+          return '**glob'
         default:
           return ''
       }
     })
+    const globGeneratorMock = jest.fn(async function* globGenerator() {
+      yield '/mock'
+    })
+
+    globCreateMock.mockImplementation(
+      async () =>
+        ({
+          globGenerator: globGeneratorMock
+        }) as unknown as glob.Globber
+    )
 
     await main.run()
     expect(runMock).toHaveReturned()
 
     // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
-    expect(errorMock).not.toHaveBeenCalled()
-  })
-
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation(name => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
-        default:
-          return ''
-      }
+    expect(globCreateMock).toHaveBeenCalledWith('**glob', {
+      followSymbolicLinks: true
     })
+    expect(globGeneratorMock).toHaveBeenCalled()
 
-    await main.run()
-    expect(runMock).toHaveReturned()
-
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
-    )
+    expect(debugMock).toHaveBeenNthCalledWith(1, 'Got file: /mock')
     expect(errorMock).not.toHaveBeenCalled()
+    expect(setFailedMock).not.toHaveBeenCalled()
   })
 })
